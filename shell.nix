3f97ed6c73c3778c175e730a2b1e232eeeaa55a0
Ian Macalinao <ian@saber.so>
2022-04-17 02:26:01 -0500

    nixpkgs-fmt

--- shell.nix
+++ shell.nix
@@ -4,7 +4,7 @@ pkgs.stdenvNoCC.mkDerivation {
   buildInputs = with pkgs;
     (pkgs.lib.optionals pkgs.stdenv.isLinux ([
       udev
-    ])) ++ [ ci cargo-deps gh spl-token-cli ]
+    ])) ++ [ ci cargo-deps gh spl-token-cli nixpkgs-fmt ]
     ++ (pkgs.lib.optionals pkgs.stdenv.isDarwin [
       pkgs.darwin.apple_sdk.frameworks.AppKit
       pkgs.darwin.apple_sdk.frameworks.IOKit
